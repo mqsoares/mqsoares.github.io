980eadd1b3395ef2c93a9b718f0a0dab0331fd68
Miqueias Soares <mqseraos@gmail.com>
2022-03-28 23:12:51 -0300

    uptates

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,20 @@
+const toggle = document.querySelectorAll('.toggle')
+const nav = document.querySelector('.header-container')
+
+for (const element of toggle) {    
+    element.addEventListener('click', function() {      
+        nav.classList.toggle('active')
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('active');
+    })
+}
+
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     setWrapperSize: true,
@@ -28,4 +45,46 @@ scrollReveal.reveal(`
     #project .project-title, #project .project-gh,
     #contact .contact-title, #contact .contact-text, #contact .contact-links
     `, 
-    { interval: 100 })
+    { interval: 100 }
+)
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop () {
+    if (window.scrollY >= 560) {
+    backToTopButton.classList.add('active')
+    } else {
+    backToTopButton.classList.remove('active')
+    }
+}
+
+const sections = document.querySelectorAll('section[id]')
+
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {        
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+    
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    
+        if (checkpointStart && checkpointEnd) {
+          document 
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+        } else {
+          document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
+
+
+window.addEventListener('scroll', function() {
+    backToTop ()
+    activateMenuAtCurrentSection()
+})
