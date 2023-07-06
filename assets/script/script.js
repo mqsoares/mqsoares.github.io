a929febe3f1156571b8ae807cf596d305badd3de
Miquéias Soares <mqseraos@gmail.com>
2023-07-05 21:25:57 -0300

    Reveal about

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,17 @@
+window.onload = function() {
+    const reveal = ScrollReveal({
+        origin: 'top',
+        distance: '30rem',
+        duration: 2500,
+        reset: true
+    })
+    reveal.reveal(`.title, .about-image, .about-write `)
+    ScrollReveal().reveal('.about-write', {delay: 500 })
+};
+
+
 const toggle = document.querySelectorAll('.toggle');
 const nav = document.querySelector('.header-container');
-const links = document.querySelectorAll('nav ul li a');
 
 for (const element of toggle) {    
     element.addEventListener('click', function() {      
@@ -8,79 +19,6 @@ for (const element of toggle) {
     })
 }
 
-for (const link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('active');
-    })
-}
 
-// const swiper = new Swiper('.swiper', {
-//     slidesPerView: 1,
-//     setWrapperSize: true,
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-//     mousewheel: true,
-//     keyboard: true,
-//     breakpoints: {
-//         775: {
-//           slidesPerView: 2,
-//           setWrapperSize: true
-//         }
-//     }
-// })
 
-const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-})
-  
-scrollReveal.reveal(`
-    #home .content-main-image, #home .content-main-written,
-    #about .about-title, #about .content-about,
-    #contact .contact-title, #contact .contact-text, #contact .contact-links
-    `, 
-    { interval: 100 }
-)
 
-const backToTopButton = document.querySelector('.back-to-top')
-
-function backToTop () {
-    if (window.scrollY >= 560) {
-    backToTopButton.classList.add('active')
-    } else {
-    backToTopButton.classList.remove('active')
-    }
-}
-
-const sections = document.querySelectorAll('section[id]')
-
-function activateMenuAtCurrentSection() {
-    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-    for (const section of sections) {        
-        const sectionTop = section.offsetTop
-        const sectionHeight = section.offsetHeight
-        const sectionId = section.getAttribute('id')
-    
-        const checkpointStart = checkpoint >= sectionTop
-        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-    
-        if (checkpointStart && checkpointEnd) {
-          document 
-            .querySelector('nav ul li a[href*=' + sectionId + ']')
-            .classList.add('active')
-        } else {
-          document
-            .querySelector('nav ul li a[href*=' + sectionId + ']')
-            .classList.remove('active')
-        }
-    }
-}
-
-window.addEventListener('scroll', function() {
-    backToTop ()
-    activateMenuAtCurrentSection()
-})
