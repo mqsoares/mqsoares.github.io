0a5ed08750cf77af6e38aa4403e88846b272af79
Miquéias Soares <mqseraos@gmail.com>
2023-07-09 12:26:43 -0300

    Animation typing subtitle

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,11 @@
-window.onload = function() {
-    const reveal = ScrollReveal({
-        origin: 'top',
-        distance: '30rem',
-        duration: 2500,
-        reset: true
-    })
-    reveal.reveal(`.title, .about-image, .about-write `)
-    ScrollReveal().reveal('.about-write', {delay: 500 })
-};
-
+const reveal = ScrollReveal({
+    origin: 'top',
+    distance: '1rem',
+    duration: 1800,
+    reset: true
+})
+reveal.reveal(`.title, .about-image, .about-write `)
+ScrollReveal().reveal('.about-write', { delay: 100 })
 
 const toggle = document.querySelectorAll('.toggle');
 const nav = document.querySelector('.header-container');
@@ -18,7 +15,3 @@ for (const element of toggle) {
         nav.classList.toggle('active')
     })
 }
-
-
-
-
