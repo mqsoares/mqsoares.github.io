12ea8224f9e38dc3ebbf05702841abe6dff2e57f
Miquéias Soares <mqseraos@gmail.com>
2023-08-13 16:44:42 -0300

    Update scroll reveal

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,10 @@
 const reveal = ScrollReveal({
     origin: 'top',
     distance: '1rem',
-    duration: 1800,
+    duration: 1300,
     reset: true
 })
-reveal.reveal(`.title, .about-image, .about-write `)
-ScrollReveal().reveal('.about-write', { delay: 100 })
+reveal.reveal(`.title, .about-image, .about-write, .footer-links-contacts`)
 
 const toggle = document.querySelectorAll('.toggle');
 const nav = document.querySelector('.header-container');
