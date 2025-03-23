8188900183fff38a5f8fdc2bc927460fbfceee99
Miquéias Soares <mqseraos@gmail.com>
2025-03-23 16:22:51 -0300

    update project

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,3 @@
-const reveal = ScrollReveal({
-    origin: 'top',
-    distance: '1rem',
-    duration: 1300,
-    reset: true
-})
-reveal.reveal(`.about-title, .about-image, .about-write, .footer-links-contacts`)
-
-const toggle = document.querySelectorAll('.toggle');
-const nav = document.querySelector('.header-container');
-
-for (const element of toggle) {    
-    element.addEventListener('click', function() {      
-        nav.classList.toggle('active')
-    })
-}
-
 console.info(`
 
   █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
